c63db6610eac44e5db63e4965ac1a2ba263168d1
tintun2602 <tintunn26@gmail.com>
2023-03-30 22:01:44 +0200

    configuration

---
+++ portfolio/main.js
@@ -0,0 +1,33 @@
+import * as THREE from "three";
+import "./style.css";
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.SphereGeometry(3, 64, 64);
+const material = new THREE.MeshStandardMaterial({
+  color: "#00ff83",
+});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+// Light
+const light = new THREE.PointLight(0xffffff, 1, 100);
+light.position.set(10, 10, 10);
+scene.add(light);
+
+//camera
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0);
+camera.position.z = 20;
+scene.add(camera);
+
+// Renderer
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
