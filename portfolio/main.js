cd9d2df81bf869a661b04c613e741391051419c6
tintun2602 <tintunn26@gmail.com>
2023-03-30 22:30:38 +0200

    cube

--- portfolio/main.js
+++ portfolio/main.js
@@ -2,32 +2,31 @@ import * as THREE from "three";
 import "./style.css";
 
 const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
-const geometry = new THREE.SphereGeometry(3, 64, 64);
-const material = new THREE.MeshStandardMaterial({
-  color: "#00ff83",
-});
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-// Sizes
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-};
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
-// Light
-const light = new THREE.PointLight(0xffffff, 1, 100);
-light.position.set(10, 10, 10);
-scene.add(light);
+camera.position.z = 5;
 
-//camera
-const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0);
-camera.position.z = 20;
-scene.add(camera);
+function animate() {
+  requestAnimationFrame(animate);
 
-// Renderer
-const canvas = document.querySelector(".webgl");
-const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
